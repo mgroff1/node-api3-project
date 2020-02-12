2c20f750297527781d2f3d13c512a258c9c5ef62
Mickey R. Groff <mick.groff@gmail.com>
2020-02-12 18:10:06 +0000

    --mvp--

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code away!
+require('dotenv').config();
+
+const server = require('./server');
+
+const port = process.env.PORT;
+
+server.listen(5000, () => {
+    console.log(`Server is running on http://localhost:${port} ^.^`)
+})
